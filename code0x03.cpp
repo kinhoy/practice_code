d5ab6faa8df374c70eb69b58449e2e15c9a8bd95
kinhoy <kinhoy97@gmail.com>
2021-03-10 21:02:40 +0800

    code0x03

---
+++ code0x03.cpp
@@ -0,0 +1,42 @@
+/* 1010 Radix */
+#include <iostream>
+#include <cctype>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+long long convert(string n, long long radix) {
+    long long sum = 0;
+    int index = 0, temp = 0;
+    for (auto it = n.rbegin(); it != n.rend(); it++) {
+        temp = isdigit(*it) ? *it - '0' : *it - 'a' + 10;
+        sum += temp * pow(radix, index++);
+    }
+    return sum;
+}
+long long find_radix(string n, long long num) {
+    char it = *max_element(n.begin(), n.end());
+    //进制下限即为 字符串中最高位 如 1090 - 10 ； 2a - a(10)+1
+    long long low = (isdigit(it) ? it - '0': it - 'a' + 10) + 1;
+    //进制上限即为 下限值与另一数值(十进制表示)取最大值
+    long long high = max(num, low);
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long t = convert(n, mid);
+        if (t < 0 || t > num) high = mid - 1;
+        else if (t == num) return mid;
+        else low = mid + 1;
+    }
+    return -1;
+}
+int main() {
+    string n1, n2;
+    long long tag = 0, radix = 0, result_radix;
+    cin >> n1 >> n2 >> tag >> radix;
+    result_radix = tag == 1 ? find_radix(n2, convert(n1, radix)) : find_radix(n1, convert(n2, radix));
+    if (result_radix != -1) {
+        printf("%lld", result_radix);
+    } else {
+        printf("Impossible");
+    }   
+    return 0;
+}
