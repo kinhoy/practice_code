f285734c844ef7719a9fdc5d22d9e0b9e43ed0b6
kinhoy <kinhoy97@gmail.com>
2021-04-14 22:45:55 +0800

    0x36

--- 1071_Speech_Patterns.cpp
+++ 1071_Speech_Patterns.cpp
@@ -1,35 +1,63 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     string s, t;
+//     getline(cin, s);
+//     map<string, int> cnt;
+//     for (int i = 0; i < s.length(); i++)
+//     {
+//         if (isalnum(s[i])) //合法字符
+//         {
+//             s[i] = tolower(s[i]);
+//             t += s[i]; //储存单词
+//         }
+//         if (!isalnum(s[i]) || i == s.length() - 1)
+//         {
+//             if (t.length() != 0)
+//                 cnt[t]++; //储存了一个单词
+//             t = "";       //t清空
+//         }
+//     }
+//     //寻找最多的那个
+//     int maxn = 0;
+//     for (auto it = cnt.begin(); it != cnt.end(); it++)
+//     {
+//         if (it->second > maxn)
+//         {
+//             maxn = it->second;
+//             t = it->first;
+//         }
+//     }
+//     cout << t << " " << maxn << endl;
+//     return 0;
+// }
 #include <bits/stdc++.h>
 using namespace std;
-
+map<string, int> cnt;
 int main()
 {
-    string s, t;
+    string s, buf;
     getline(cin, s);
-    map<string, int> cnt;
     for (int i = 0; i < s.length(); i++)
-    {
-        if (isalnum(s[i])) //合法字符
-        {
+        if (isalnum(s[i]))
             s[i] = tolower(s[i]);
-            t += s[i]; //储存单词
-        }
-        if (!isalnum(s[i]) || i == s.length() - 1)
-        {
-            if (t.length() != 0)
-                cnt[t]++; //储存了一个单词
-            t = "";       //t清空
-        }
-    }
-    //寻找最多的那个
+        else
+            s[i] = ' ';
+    stringstream ss(s);
+    while (ss >> buf)
+        cnt[buf]++;
     int maxn = 0;
     for (auto it = cnt.begin(); it != cnt.end(); it++)
     {
         if (it->second > maxn)
         {
             maxn = it->second;
-            t = it->first;
+            buf = it->first;
         }
     }
-    cout << t << " " << maxn << endl;
+    cout << buf << " " << maxn << endl;
+    system("pause");
     return 0;
 }
