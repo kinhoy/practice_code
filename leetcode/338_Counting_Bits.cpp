74531129131c0117473bb66e7d7f6af12116baa4
kinhoy <kinhoy97@gmail.com>
2021-05-24 23:55:07 +0800

    0x59

---
+++ leetcode/338_Counting_Bits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> countBits(int n)
+    {
+        vector<int> result(n + 1);
+        for (int i = 1; i <= n; i++)
+        {
+            result[i] = result[i >> 1] + (i & 1);
+        }
+        return result;
+    }
+};
